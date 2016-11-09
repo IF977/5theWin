1450f87c974a2dce774f4047963eaf51a5a09d17
Isa <iill@bepid.cin.ufpe.br>
2016-11-09 21:57:02 +0000

    Adequando ao Heroku

---
+++ TESTE/teste_dados.rb
@@ -0,0 +1,34 @@
+def leDadosAbertos
+    require 'open-uri'
+    require 'csv'
+    dados = open('http://dados.recife.pe.gov.br/storage/f/2016-10-19T161509/156_diario.csv').read
+    lines = dados.split("\r\n")
+    dados_processados = []
+    lines.each do |line|
+        dados_processados << line.split(";")
+    end
+    dados_de_interesse = { GRUPOSERVICO_DESCRICAO: nil, SERVICO_DESCRICAO: nil, LOGRADOURO: nil, NUMERO: nil, BAIRRO: nil, DATA_DEMANDA: nil, SITUACAO: nil, DATA_ULT_SITUACAO: nil}
+    lista_de_dados = dados_processados.delete_at(0)
+    dados_de_interesse.each do |dado,posicao|
+        dados_de_interesse[dado] = lista_de_dados.index(dado.to_s)
+    end
+    dados_finais = []
+    dados_processados.each do |array|
+        dados_finais << Hash.new()
+        dados_de_interesse.each do |dado,posicao|
+            dados_finais.last[dado] = array[posicao]
+        end
+    end
+    
+    @info = "[ "
+    
+    dados_finais.each do |dado|
+        @info += "{address: '#{dado[:LOGRADOURO]}, #{dado[:NUMERO]}, #{dado[:BAIRRO]}', "
+        @info += "grupo: '#{dado[:GRUPOSERVICO_DESCRICAO]}', "
+        @info += "desc: '#{dado[:SERVICO_DESCRICAO]}', "
+        @info += "data: '#{dado[:DATA_DEMANDA]}', "
+        @info += "situacao: '#{dado[:SITUACAO]}'},"
+    end
+    @info.chop!
+    @info += "]"
+end
